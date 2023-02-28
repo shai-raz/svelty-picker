e4cb9ef1971741986471045c6c50359ad476f71b
Shai Raz <shayy9999@gmail.com>
2023-02-28 16:55:18 +0200

    package

---
+++ package/i18n/index.d.ts
@@ -0,0 +1,217 @@
+export namespace cz {
+    const days: string[];
+    const daysShort: string[];
+    const daysMin: string[];
+    const months: string[];
+    const monthsShort: string[];
+    const meridiem: string[];
+    const suffix: string[];
+    const todayBtn: string;
+    const clearBtn: string;
+    const timeView: string;
+    const backToDate: string;
+}
+export namespace cs { }
+export namespace de {
+    const days_1: string[];
+    export { days_1 as days };
+    const daysShort_1: string[];
+    export { daysShort_1 as daysShort };
+    const daysMin_1: string[];
+    export { daysMin_1 as daysMin };
+    const months_1: string[];
+    export { months_1 as months };
+    const monthsShort_1: string[];
+    export { monthsShort_1 as monthsShort };
+    const meridiem_1: string[];
+    export { meridiem_1 as meridiem };
+    const suffix_1: string[];
+    export { suffix_1 as suffix };
+    const todayBtn_1: string;
+    export { todayBtn_1 as todayBtn };
+    const clearBtn_1: string;
+    export { clearBtn_1 as clearBtn };
+    const timeView_1: string;
+    export { timeView_1 as timeView };
+    const backToDate_1: string;
+    export { backToDate_1 as backToDate };
+}
+export namespace en {
+    const days_2: string[];
+    export { days_2 as days };
+    const daysShort_2: string[];
+    export { daysShort_2 as daysShort };
+    const daysMin_2: string[];
+    export { daysMin_2 as daysMin };
+    const months_2: string[];
+    export { months_2 as months };
+    const monthsShort_2: string[];
+    export { monthsShort_2 as monthsShort };
+    const meridiem_2: string[];
+    export { meridiem_2 as meridiem };
+    const suffix_2: string[];
+    export { suffix_2 as suffix };
+    const todayBtn_2: string;
+    export { todayBtn_2 as todayBtn };
+    const clearBtn_2: string;
+    export { clearBtn_2 as clearBtn };
+    const timeView_2: string;
+    export { timeView_2 as timeView };
+    const backToDate_2: string;
+    export { backToDate_2 as backToDate };
+}
+export namespace es {
+    const days_3: string[];
+    export { days_3 as days };
+    const daysShort_3: string[];
+    export { daysShort_3 as daysShort };
+    const daysMin_3: string[];
+    export { daysMin_3 as daysMin };
+    const months_3: string[];
+    export { months_3 as months };
+    const monthsShort_3: string[];
+    export { monthsShort_3 as monthsShort };
+    const meridiem_3: string[];
+    export { meridiem_3 as meridiem };
+    const suffix_3: string[];
+    export { suffix_3 as suffix };
+    const todayBtn_3: string;
+    export { todayBtn_3 as todayBtn };
+    const clearBtn_3: string;
+    export { clearBtn_3 as clearBtn };
+    const timeView_3: string;
+    export { timeView_3 as timeView };
+    const backToDate_3: string;
+    export { backToDate_3 as backToDate };
+}
+export namespace fa {
+    const days_4: string[];
+    export { days_4 as days };
+    const daysShort_4: string[];
+    export { daysShort_4 as daysShort };
+    const daysMin_4: string[];
+    export { daysMin_4 as daysMin };
+    const months_4: string[];
+    export { months_4 as months };
+    const monthsShort_4: string[];
+    export { monthsShort_4 as monthsShort };
+    const meridiem_4: string[];
+    export { meridiem_4 as meridiem };
+    const suffix_4: string[];
+    export { suffix_4 as suffix };
+    const todayBtn_4: string;
+    export { todayBtn_4 as todayBtn };
+    const clearBtn_4: string;
+    export { clearBtn_4 as clearBtn };
+    const timeView_4: string;
+    export { timeView_4 as timeView };
+    const backToDate_4: string;
+    export { backToDate_4 as backToDate };
+}
+export namespace id {
+    const days_5: string[];
+    export { days_5 as days };
+    const daysShort_5: string[];
+    export { daysShort_5 as daysShort };
+    const daysMin_5: string[];
+    export { daysMin_5 as daysMin };
+    const months_5: string[];
+    export { months_5 as months };
+    const monthsShort_5: string[];
+    export { monthsShort_5 as monthsShort };
+    const meridiem_5: string[];
+    export { meridiem_5 as meridiem };
+    const suffix_5: string[];
+    export { suffix_5 as suffix };
+    const todayBtn_5: string;
+    export { todayBtn_5 as todayBtn };
+    const clearBtn_5: string;
+    export { clearBtn_5 as clearBtn };
+    const timeView_5: string;
+    export { timeView_5 as timeView };
+    const backToDate_5: string;
+    export { backToDate_5 as backToDate };
+}
+export namespace jp {
+    const days_6: string[];
+    export { days_6 as days };
+    const daysShort_6: string[];
+    export { daysShort_6 as daysShort };
+    const daysMin_6: string[];
+    export { daysMin_6 as daysMin };
+    const months_6: string[];
+    export { months_6 as months };
+    const monthsShort_6: string[];
+    export { monthsShort_6 as monthsShort };
+    const meridiem_6: string[];
+    export { meridiem_6 as meridiem };
+    const suffix_6: string[];
+    export { suffix_6 as suffix };
+    const todayBtn_6: string;
+    export { todayBtn_6 as todayBtn };
+    const clearBtn_6: string;
+    export { clearBtn_6 as clearBtn };
+    const timeView_6: string;
+    export { timeView_6 as timeView };
+    const backToDate_6: string;
+    export { backToDate_6 as backToDate };
+}
+export namespace sk {
+    const days_7: string[];
+    export { days_7 as days };
+    const daysShort_7: string[];
+    export { daysShort_7 as daysShort };
+    const daysMin_7: string[];
+    export { daysMin_7 as daysMin };
+    const months_7: string[];
+    export { months_7 as months };
+    const monthsShort_7: string[];
+    export { monthsShort_7 as monthsShort };
+    const meridiem_7: string[];
+    export { meridiem_7 as meridiem };
+    const suffix_7: string[];
+    export { suffix_7 as suffix };
+    const todayBtn_7: string;
+    export { todayBtn_7 as todayBtn };
+    const clearBtn_7: string;
+    export { clearBtn_7 as clearBtn };
+    const timeView_7: string;
+    export { timeView_7 as timeView };
+    const backToDate_7: string;
+    export { backToDate_7 as backToDate };
+}
+export namespace ko {
+    const days_8: string[];
+    export { days_8 as days };
+    const daysShort_8: string[];
+    export { daysShort_8 as daysShort };
+    const daysMin_8: string[];
+    export { daysMin_8 as daysMin };
+    const months_8: string[];
+    export { months_8 as months };
+    const monthsShort_8: string[];
+    export { monthsShort_8 as monthsShort };
+    const meridiem_8: string[];
+    export { meridiem_8 as meridiem };
+    const suffix_8: string[];
+    export { suffix_8 as suffix };
+    const todayBtn_8: string;
+    export { todayBtn_8 as todayBtn };
+    const clearBtn_8: string;
+    export { clearBtn_8 as clearBtn };
+    const timeView_8: string;
+    export { timeView_8 as timeView };
+    const backToDate_8: string;
+    export { backToDate_8 as backToDate };
+}
+declare namespace _default {
+    export { cz };
+    export { de };
+    export { en };
+    export { es };
+    export { id };
+    export { jp };
+    export { sk };
+    export { ko };
+}
+export default _default;
---
+++ package/i18n/index.js
@@ -0,0 +1,137 @@
+// always keep sorted alphabetically
+export const cz = {
+  days:        ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+  daysShort:   ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+  daysMin:     ['Ne', 'Po', 'Út', 'St', 'Čt', 'Pá', 'So', 'Ne'],
+  months:      ['Leden', 'Únor', 'Březen', 'Duben', 'Květen', 'Červen', 'Červenec', 'Srpen', 'Září', 'Říjen', 'Listopad', 'Prosinec'],
+  monthsShort: ['Led', 'Úno', 'Bře', 'Dub', 'Kvě', 'Čer', 'Čer', 'Srp', 'Zář', 'Říj', 'List', 'Pro'],
+  meridiem:    ['am', 'pm'],
+  suffix:      ['st', 'nd', 'rd', 'th'],
+  todayBtn:    'Dnes',
+  clearBtn:    'Smazat',
+  timeView:    'Zobrazit hodiny',
+  backToDate:  'Zpátky na kalendář'
+}
+// FUTURE: remove this BC on major version release
+export const cs = cz;
+
+// German translation by emroc GmbH
+export const de = {
+  days:        ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag', 'Sonntag'],
+  daysShort:   ['Son', 'Mon', 'Die', 'Mie', 'Don', 'Fre', 'Sam', 'Son'],
+  daysMin:     ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa', 'So'],
+  months:      ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'],
+  monthsShort: ['Jan', 'Feb', 'Mär', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez'],
+  meridiem:    ['am', 'pm'],
+  suffix:      ['', '', '', ''],
+  todayBtn:    'Heute',
+  clearBtn:    'Zurücksetzen',
+  timeView:    'Zeitansicht anzeigen',
+  backToDate:  'Zurück zur Kalenderansicht'
+}
+
+export const en = {
+  days:        ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+  daysShort:   ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+  daysMin:     ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'],
+  months:      ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+  monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+  meridiem:    ['am', 'pm'],
+  suffix:      ['st', 'nd', 'rd', 'th'],
+  todayBtn:    'Today',
+  clearBtn:    'Clear',
+  timeView:    'Show time view',
+  backToDate:  'Back to calendar view'
+}
+
+// Spanish translation by markoan
+export const es = {
+  days:        ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'],
+  daysShort:   ['Dom', 'Lun', 'Mar', 'Mié', 'Jue', 'Vie', 'Sáb', 'Dom'],
+  daysMin:     ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sa', 'Do'],
+  months:      ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+  monthsShort: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
+  meridiem:    ['am', 'pm'],
+  suffix:      ['o', '', '', ''],
+  todayBtn:    'Hoy',
+  clearBtn:    'Borrar',
+  timeView:    'Mostrar hora',
+  backToDate:  'Regresar al calendario'
+}
+
+// Persian (Farsi) translation by sadegh19b
+export const fa = {
+  days:        ['یکشنبه', 'دوشنبه', 'سه‌شنبه‌', 'چهارشنبه', 'پنج‌شنبه', 'جمعه', 'شنبه', 'یکشنبه'],
+  daysShort:   ['یکش', 'دوش', 'سه‌ش', 'چهار', 'پنج', 'جمع', 'شنب', 'یکش'],
+  daysMin:     ['یک', 'دو', 'سه', 'چا', 'پن', 'جم', 'شن', 'یک'],
+  months:      ['ژانویه', 'فوریه', 'مارس', 'آپریل', 'می', 'ژوئن', 'جولای', 'آگوست', 'سپتامبر', 'اکتبر', 'نوامبر', 'دسامبر'],
+  monthsShort: ['ژان', 'فور', 'مار', 'آپر', 'می', 'ژو', 'جول', 'آگو', 'سپت', 'اکت', 'نوا', 'دسا'],
+  meridiem:    ['ق.ض', 'ب.ض'],
+  suffix:      ['st', 'nd', 'rd', 'th'],
+  todayBtn:    'امروز',
+  clearBtn:    'پاک‌کردن',
+  timeView:    'نمایش بخش زمان',
+  backToDate:  'بازگشت به بخش تقویم'
+}
+
+// Indonesian translation
+export const id = {
+  days:        ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu', 'Minggu'],
+  daysShort:   ['Min', 'Sen', 'Sel', 'Rab', 'Kam', 'Jum', 'Sab', 'Min'],
+  daysMin:     ['Mn', 'Sn', 'Sl', 'Rb', 'Km', 'Jm', 'Sb', 'Mn'],
+  months:      ['januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'],
+  monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun', 'Jul', 'Agu', 'Sep', 'Okt', 'Nov', 'Des'],
+  meridiem:    ['am', 'pm'],
+  suffix:      ['st', 'nd', 'rd', 'th'],
+  todayBtn:    'Hari Ini',
+  clearBtn:    'Hapus',
+  timeView:    'Tampilkan tampilan waktu',
+  backToDate:  'Kembali ke tampilan kalender'
+}
+
+// Japanese translation by aska
+export const jp = {
+  days:        ['日曜日', '月曜日', '火曜日', '水曜日', '木曜日', '金曜日', '土曜日', '日曜日'],
+  daysShort:   ['日曜', '月曜', '火曜', '水曜', '木曜', '金曜', '土曜', '日曜'],
+  daysMin:     ['日', '月', '火', '水', '木', '金', '土', '日'],
+  months:      ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'],
+  monthsShort: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'],
+  meridiem:    ['am', 'pm'],
+  suffix:      ['', '', '', ''],
+  todayBtn:    '今日',
+  clearBtn:    'クリア',
+  timeView:    'タイムを表示',
+  backToDate:  'カレンダーに戻る'
+}
+
+export const sk = {
+  days:        ['Nedeľa', 'Pondelok', 'Utorok', 'Streda', 'Štvrtok', 'Piatok', 'Sobota', 'Nedeľa'],
+  daysShort:   ['Ned', 'Pon', 'Uto', 'Str', 'Štv', 'Pia', 'Sob', 'Ned'],
+  daysMin:     ['Ne', 'Po', 'Ut', 'St', 'Št', 'Pi', 'So', 'Ne'],
+  months:      ['Január', 'Február', 'Marec', 'Apríl', 'Máj', 'Jún', 'Júl', 'August', 'September', 'Október', 'November', 'December'],
+  monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'Máj', 'Jún', 'Júl', 'Aug', 'Sep', 'Okt', 'Nov', 'Dec'],
+  meridiem:    ['am', 'pm'],
+  suffix:      ['st', 'nd', 'rd', 'th'],
+  todayBtn:    'Dnes',
+  clearBtn:    'Zmazať',
+  timeView:    'Zobraziť hodiny',
+  backToDate:  'Späť na kalendár'
+}
+
+export const ko = {
+  days:        ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일', '일요일'],
+  daysShort:   ['일', '월', '화', '수', '목', '금', '토', '일'],
+  daysMin:     ['일', '월', '화', '수', '목', '금', '토', '일'],
+  months:      ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+  monthsShort: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+  meridiem:    ['오전', '오후'],
+  suffix:      ['', '', '', ''],
+  todayBtn:    '오늘',
+  clearBtn:    '지우기',
+  timeView:    '시계보기',
+  backToDate:  '달력보기'
+}
+
+export default {
+  cz, de, en, es, id, jp, sk, ko
+}
